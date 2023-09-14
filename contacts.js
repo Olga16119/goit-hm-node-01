9d00d9404ca26380fb20729ec86d51f6f08396ec
Olga16119 <tkachenko93@ukr.net>
2023-09-14 21:27:08 +0200

    json.parse contacts

--- contacts.js
+++ contacts.js
@@ -5,21 +5,20 @@ const { nanoid } = require("nanoid");
 const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 const listContacts = async () => {
-  const data = fs.readFile(contactsPath, "utf-8");
+  const dataContacts = await fs.readFile(contactsPath, "utf-8");
+  const data = JSON.parse(dataContacts);
   return data;
 };
 
 const getContactById = async (id) => {
-  const data = await listContacts();
-  const contacts = JSON.parse(data);
+  const contacts = await listContacts();
 
   const result = contacts.find((item) => item.id === id);
   return result || null;
 };
 
 const addContact = async (data) => {
-  const dataContacts = await listContacts();
-  const contacts = JSON.parse(dataContacts);
+  const contacts = await listContacts();
   const newContact = {
     id: nanoid(),
     ...data,
@@ -30,8 +29,7 @@ const addContact = async (data) => {
 };
 
 const deleteContact = async (id) => {
-  const data = await listContacts();
-  const contacts = JSON.parse(data);
+  const contacts = await listContacts();
   const index = contacts.findIndex((item) => item.id === id);
   if (index === -1) {
     return null;
